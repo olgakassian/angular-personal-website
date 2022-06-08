f19a9455db2729f1412d866e918395b371b59ed6
Olga Kassian <olga.kassian@evident.nl>
2022-06-08 18:20:03 +0100

    Fixed Mobile Nav Bar - Imported Bootstrap JS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ContactFormComponent],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
+
+    HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.scss'],
+})
+export class ContactFormComponent implements OnInit {
+  contactForm: FormGroup = this.fb.group({
+    name: [''],
+    email: ['', Validators.required],
+    message: [''],
+  });
+
+  confirmationUrl: SafeResourceUrl;
+
+  constructor(
+    private fb: FormBuilder,
+    private httpClient: HttpClient,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit() {}
+
+  submitForm() {
+    //   if (this.contactForm.valid) {
+    //     this.httpClient
+    //       .post(
+    //         'https://mailthis.to/olgakassian@gmail.com',
+    //         this.contactForm.value,
+    //         { responseType: 'text' }
+    //       )
+    //       // .subscribe((data) => {
+    //       //   // console.log(data);
+    //       //   location.href = 'https://mailthis.to/confirm';
+    //       // });
+    //   }
+    // }
+  }
+}
